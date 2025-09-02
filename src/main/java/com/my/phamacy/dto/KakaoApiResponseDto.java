6240c17485b201bd4334e6fb63989ba8938b9af9
Johnbyeon <ghks0115@gmail.com>
2025-09-02 13:16:54 +0900

    kakao_API 서비스 url 구성완료

--- src/main/java/com/my/phamacy/dto/KakaoApiResponseDto.java
+++ src/main/java/com/my/phamacy/dto/KakaoApiResponseDto.java
@@ -1,9 +1,11 @@
 package com.my.phamacy.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 
 import java.util.List;
 
+@Data
 public class KakaoApiResponseDto {
     @JsonProperty("documents")
     private List<DocumentDto> documentDtoList;
