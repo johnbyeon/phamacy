ae5941db833e2a82cccabc42c2c38fc47e092891
Johnbyeon <ghks0115@gmail.com>
2025-09-02 14:44:07 +0900

    카테고리 검색 서비스 구현

--- src/test/java/com/my/phamacy/service/KakaoAddressSearchServiceTest.java
+++ src/test/java/com/my/phamacy/service/KakaoAddressSearchServiceTest.java
@@ -11,6 +11,9 @@ import static org.junit.jupiter.api.Assertions.*;
 class KakaoAddressSearchServiceTest {
     @Autowired
     KakaoAddressSearchService kakaoaddressSearchService;
+
+    @Autowired
+    KakaoCategorySearchService kakaoCategorySearchService;
     @Test
     @DisplayName("API KEY TEST")
     void Test(){
@@ -23,6 +26,17 @@ class KakaoAddressSearchServiceTest {
        KakaoApiResponseDto dto = new KakaoApiResponseDto();
              dto =  kakaoaddressSearchService.requestAddressSearch("강남대로 450");
         System.out.println(dto.toString());
+    }
+
+    @Test
+    @DisplayName("Search Category Test")
+    void category(){
+        KakaoApiResponseDto dto= new KakaoApiResponseDto();
+        double x = 127.026692446306;
+        double y = 37.4987750083767;
+        double radius = 1000;
+        dto = kakaoCategorySearchService.resultCategorySearch(x,y,radius);
+        System.out.println(dto);
     }
 
 
